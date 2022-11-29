a4d22283ec430cece5ba14d486e7c25d2c208983
visiblestars <hakkim@visiblestarsksa.com>
2022-11-29 12:55:57 +0000

    updated in slaveBranch 3

--- src/test/java/testgitproject/WelcomeTest.java
+++ src/test/java/testgitproject/WelcomeTest.java
@@ -6,5 +6,6 @@ public class WelcomeTest {
 	System.out.println("welcome");
 	System.out.println("Added code in slavebranch  3");
 	
+	System.out.println("Added  other code in slavebranch  3");
 	}
 }
