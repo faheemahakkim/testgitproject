0481147641f5122e31752dc16c7cb178aaf1933a
visiblestars <hakkim@visiblestarsksa.com>
2022-11-29 11:59:35 +0000

    add +sign

--- src/test/java/testgitproject/NumberTest.java
+++ src/test/java/testgitproject/NumberTest.java
@@ -3,7 +3,11 @@ package testgitproject;
 public class NumberTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			System.out.println(i++);
+
+		}
+			
 
 	}
 
